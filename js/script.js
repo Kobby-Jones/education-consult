432e295e77474ed180401b90ca600a59d89d75df
Cobbina Emmanuel <kobbyj099@gmail.com>
2023-01-11 15:10:31 -0800

    Links added to the anchor tags

--- js/script.js
+++ js/script.js
@@ -59,17 +59,3 @@ $(".owl-carousel2").owlCarousel({
   }
 });
 
-const counters = document.querySelectorAll('.counter');
-counters.forEach((counter) => {
-  counter.innerText = '0';
-  const updateCounter = () => {
-    const target =  +counter.getAttribute('data-val');
-    const c = +counter.innerText;
-    const increment = target / 200;
-    if (c < target) {
-      counter.innerText = `${Math.ceil(c + increment)}`;
-      setTimeout(updateCounter, 1);
-    }
-  }
-  updateCounter();
-});
